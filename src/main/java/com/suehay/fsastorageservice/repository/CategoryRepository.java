bfeb39f501b697ccf593be107adb9cec262f5f44
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-30 14:29:34 -0400

    feat: changes on CategoryRepository

--- src/main/java/com/suehay/fsastorageservice/repository/CategoryRepository.java
+++ src/main/java/com/suehay/fsastorageservice/repository/CategoryRepository.java
@@ -1,7 +1,10 @@
 package com.suehay.fsastorageservice.repository;
 
 import com.suehay.fsastorageservice.model.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Page<Category> findAllByNameContains(Pageable pageable, String filter);
 }
