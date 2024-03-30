ea828dff21519e55249dead80f6a442cd822b4da
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-30 15:38:50 -0400

    feat: implement user details configuration

--- src/main/java/com/suehay/fsastorageservice/repository/UserRepository.java
+++ src/main/java/com/suehay/fsastorageservice/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.suehay.fsastorageservice.repository;
 import com.suehay.fsastorageservice.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
