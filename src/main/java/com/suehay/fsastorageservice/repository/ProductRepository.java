ada62a34f704b31e06c29b537a5c8bd901e47371
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-30 12:23:31 -0400

    feat: implement pageable request in repository

--- src/main/java/com/suehay/fsastorageservice/repository/ProductRepository.java
+++ src/main/java/com/suehay/fsastorageservice/repository/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.suehay.fsastorageservice.repository;
 
 import com.suehay.fsastorageservice.model.entity.Product;
+import org.springframework.data.annotation.QueryAnnotation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.category.name = :categoryName ORDER BY p.id ASC")
+    Page<Product> findAllByCategoryNameContains(Pageable pageable, String categoryName);
 }
