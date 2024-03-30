f0028d7f4e24ef42aecf1d8baa40fcbc67b027f9
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-30 14:29:52 -0400

    feat: implement methods in CategoryService

--- src/main/java/com/suehay/fsastorageservice/service/CategoryService.java
+++ src/main/java/com/suehay/fsastorageservice/service/CategoryService.java
@@ -1,4 +1,14 @@
 package com.suehay.fsastorageservice.service;
 
+import com.suehay.fsastorageservice.model.entity.Category;
+import com.suehay.fsastorageservice.model.request.GenericPageRequest;
+import com.suehay.fsastorageservice.model.response.GenericResponse;
+import org.springframework.data.domain.Page;
+
 public interface CategoryService {
+    GenericResponse<Category> saveCategory(Category category);
+
+    GenericResponse<Category> getCategoryById(String id);
+
+    GenericResponse<Page<Category>> getCategories(GenericPageRequest<String> request);
 }
---
+++ src/main/java/com/suehay/fsastorageservice/service/CategoryServiceImpl.java
@@ -0,0 +1,67 @@
+package com.suehay.fsastorageservice.service;
+
+import com.suehay.fsastorageservice.model.entity.Category;
+import com.suehay.fsastorageservice.model.request.GenericPageRequest;
+import com.suehay.fsastorageservice.model.response.GenericResponse;
+import com.suehay.fsastorageservice.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final FileStorageService fileStorageService;
+
+    @Override
+    public GenericResponse<Category> saveCategory(Category category) {
+        var response = new GenericResponse<Category>();
+        var addresses = fileStorageService.getAddressByNames(List.of(category.getImage()));
+        category.setImage(!addresses.isEmpty() ? addresses.get(0) : null);
+        response.setData(categoryRepository.save(category));
+        response.setMessage("Category saved with id: " + category.getId());
+        response.setStatus("200");
+        return response;
+    }
+
+    @Override
+    public GenericResponse<Category> getCategoryById(String id) {
+        var response = new GenericResponse<Category>();
+        var category = categoryRepository.findById(Long.parseLong(id));
+        if (category.isPresent()) {
+            response.setData(category.get());
+            response.setMessage("Category found with id: " + id);
+            response.setStatus("200");
+        } else {
+            response.setMessage("No category found with id: " + id);
+            response.setStatus("404");
+            response.setError("Not Found");
+        }
+        return response;
+    }
+
+    @Override
+    public GenericResponse<Page<Category>> getCategories(GenericPageRequest<String> request) {
+        var response = new GenericResponse<Page<Category>>();
+        if (request.getFilter() != null) {
+            response.setData(categoryRepository.findAllByNameContains(PageRequest.of(request.getPage(), request.getSize()), request.getFilter()));
+        } else {
+            response.setData(categoryRepository.findAll(PageRequest.of(request.getPage(), request.getSize())));
+        }
+        if (response.getData().isEmpty()) {
+            response.setMessage("No categories found with name: " + request.getFilter());
+            response.setStatus("404");
+            response.setError("Not Found");
+        } else {
+            response.setMessage("Categories found with name: " + request.getFilter());
+            response.setStatus("200");
+        }
+        return response;
+    }
+}
