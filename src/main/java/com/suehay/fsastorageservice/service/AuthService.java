57eed5282c8a79e71e690c1cbfced4dc11769228
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-30 15:07:53 -0400

    feat: create AuthServiceImpl

--- src/main/java/com/suehay/fsastorageservice/service/AuthService.java
+++ src/main/java/com/suehay/fsastorageservice/service/AuthService.java
@@ -1,4 +1,8 @@
 package com.suehay.fsastorageservice.service;
 
 public interface AuthService {
+    String login(String username, String password);
+    String register(String username, String password);
+    String logout(String token);
+    String refreshToken(String token);
 }
---
+++ src/main/java/com/suehay/fsastorageservice/service/AuthServiceImpl.java
@@ -0,0 +1,4 @@
+package com.suehay.fsastorageservice.service;
+
+public class AuthServiceImpl {
+}
