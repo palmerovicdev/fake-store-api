ac09f461a0685ad6775ba9e7c3be2cf93dd2b010
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-03-30 11:42:57 -0400

    feat: create UserEntity

---
+++ src/main/java/com/suehay/fsastorageservice/model/entity/User.java
@@ -0,0 +1,26 @@
+package com.suehay.fsastorageservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "image")
+    private String image;
+
+}
