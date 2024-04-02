9567d40597c4289b969d6e6a1d1723c6e67404ec
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-04-01 22:20:06 -0400

    feat: create register and login methods

--- src/main/java/com/suehay/fsastorageservice/controller/AuthenticationController.java
+++ src/main/java/com/suehay/fsastorageservice/controller/AuthenticationController.java
@@ -1,4 +1,37 @@
 package com.suehay.fsastorageservice.controller;
 
+import com.suehay.fsastorageservice.model.request.AuthenticationRequest;
+import com.suehay.fsastorageservice.model.response.AuthenticationResponse;
+import com.suehay.fsastorageservice.model.response.GenericResponse;
+import com.suehay.fsastorageservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthenticationController {
+    private final AuthService authService;
+
+    @Operation(summary = "Register a user by username and password", description = "Register a user", responses = {
+            @ApiResponse(responseCode = "200", description = "User registered successfully"),
+            @ApiResponse(responseCode = "500", description = "Error registering user")
+    })
+    @PostMapping("/register")
+    public ResponseEntity<GenericResponse<AuthenticationResponse>> register(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authService.register(authenticationRequest));
+    }
+
+    @Operation(summary = "Login a user by username and password", description = "Login a user", responses = {
+            @ApiResponse(responseCode = "200", description = "Login successful"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Error logging in user")
+    })
+    @GetMapping("/login")
+    public ResponseEntity<GenericResponse<AuthenticationResponse>> login(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authService.login(authenticationRequest));
+    }
 }
