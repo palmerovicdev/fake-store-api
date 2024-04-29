c4400ba30a56583127de284d3a2d20e8d10ca14c
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-04-29 13:49:59 -0400

    fix(swagger): fix swagger docs 2

--- src/main/java/com/suehay/fsastorageservice/config/SwaggerConfig.java
+++ src/main/java/com/suehay/fsastorageservice/config/SwaggerConfig.java
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableOpenApi
 public class SwaggerConfig {
     @Bean
     public Docket api() {
