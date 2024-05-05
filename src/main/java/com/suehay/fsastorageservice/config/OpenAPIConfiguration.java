249c01e98ffc512868ece061b918bfc28764c35b
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-05-05 17:21:03 -0400

    fix: updating swagger

--- src/main/java/com/suehay/fsastorageservice/config/OpenAPIConfiguration.java
+++ src/main/java/com/suehay/fsastorageservice/config/OpenAPIConfiguration.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,21 +13,24 @@ import java.util.List;
 @Configuration
 public class OpenAPIConfiguration {
 
+    @Value("${spring.server.url}") String SERVER_URL;
+
    @Bean
    public OpenAPI defineOpenApi() {
        var server = new Server();
-       server.setUrl("http://localhost:8080");
-       server.setDescription("Development");
+       server.setUrl(SERVER_URL);
+       server.setDescription("Fake Storage API Server. A simple API to manage categories, products and files.");
 
        var myContact = new Contact();
-       myContact.setName("Victor Palmero");
+       myContact.setName("Victor Palmero Valdes");
        myContact.setEmail("palmerodev@gmail.com");
+       myContact.setUrl("https://github.com/palmerovicdev");
 
        var information = new Info()
                .title("Fake Storage API")
                .version("1.0")
                .license(new io.swagger.v3.oas.models.info.License().name("MIT").url("https://opensource.org/licenses/MIT"))
-               .description("This API exposes endpoints to manage employees.")
+               .description("This API exposes endpoints to manage categories, products and files.")
                .contact(myContact);
        return new OpenAPI().info(information).servers(List.of(server));
    }
