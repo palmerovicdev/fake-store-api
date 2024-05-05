4540b50a6535abb7e97ef60b082bbeb2501d748d
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-05-05 16:31:50 -0400

    feat: add swagger

---
+++ src/main/java/com/suehay/fsastorageservice/config/Tasks.java
@@ -0,0 +1,19 @@
+package com.suehay.fsastorageservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Tasks {
+
+    @Value("${spring.server.url}") String SERVER_URL;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        log.info("Swagger URL: {}swagger-ui/swagger-ui.html", SERVER_URL);
+    }
+}
